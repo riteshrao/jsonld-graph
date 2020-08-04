644e711760460d1dec55c66dbf490fc22cf1c2e3
Ritesh Rao <rao.ritesh@gmail.com>
2020-08-04 09:31:34 -0700

    Bug fix: invalid id keyword check for anonymous type formatting

--- src/formatter.ts
+++ src/formatter.ts
@@ -154,12 +154,14 @@ function _expand(vertex: Vertex, options: ExpandFormatOptions = {}, visitStack: 
             } else {
                 if (!visitStack.includes(outgoing.to.iri)) {
                     const expandedOut = _expand(outgoing.to, options, visitStack);
-                    if ((options.anonymousReferences && typeof options.anonymousReferences === 'boolean' && options.anonymousReferences === true) ||
+                    if (expandedOut[JsonldKeywords.id] &&
+                        (options.anonymousReferences && typeof options.anonymousReferences === 'boolean' && options.anonymousReferences === true) ||
                         (options.anonymousReferences && typeof options.anonymousReferences !== 'boolean' && options.anonymousReferences(outgoing.from, outgoing.iri, outgoing.to))) {
+                            
                         delete expandedOut[JsonldKeywords.id];
                     }
 
-                    if (expandedOut[JsonldKeywords.id] &&
+                    if (expandedOut[JsonldKeywords.type] &&
                         (options.anonymousTypes && typeof options.anonymousTypes === 'boolean' && options.anonymousTypes === true) ||
                         (options.anonymousTypes && typeof options.anonymousTypes !== 'boolean' && options.anonymousTypes(outgoing.from, outgoing.iri, outgoing.to))) {
 
