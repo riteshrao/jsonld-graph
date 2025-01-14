e0450b7720051b553890a1f1fad5c99783b32f69
Davide Montanari <damontan@microsoft.com>
2025-01-13 22:20:42 -0800

    - Removed deprecated shortid library
    - Added nanoid library
    - Moved Typescript target to ES2021
    - Dropped support for Node.js versions < 18

--- src/graph.spec.ts
+++ src/graph.spec.ts
@@ -1,4 +1,4 @@
-import shortid from 'shortid';
+import { nanoid } from 'nanoid';
 import Edge from './edge';
 import * as errors from './errors';
 import JsonldGraph from './graph';
@@ -1059,7 +1059,7 @@ describe('JsonldGraph', () => {
 
             const graph = new JsonldGraph({
                 blankIriResolver: (vertex): string => {
-                    const name = vertex.getAttributeValue("vocab:Entity/name") || shortid();
+                    const name = vertex.getAttributeValue("vocab:Entity/name") || nanoid();
                     const parent = vertex.getIncoming().first();
                     if (!parent) {
                         return name;
@@ -1174,7 +1174,7 @@ describe('JsonldGraph', () => {
 
             const graph = new JsonldGraph({
                 blankIriResolver: (vertex): string | undefined => {
-                    const name = vertex.getAttributeValue("vocab:Entity/name") || shortid();
+                    const name = vertex.getAttributeValue("vocab:Entity/name") || nanoid();
                     return 'http://example.org/hr/instances/' + name;
                 }
             });
@@ -1234,7 +1234,7 @@ describe('JsonldGraph', () => {
 
             const graph = new JsonldGraph({
                 blankIriResolver: (vertex): string | undefined => {
-                    const name = vertex.getAttributeValue("vocab:Entity/name") || shortid();
+                    const name = vertex.getAttributeValue("vocab:Entity/name") || nanoid();
                     return 'http://example.org/hr/instances/' + name;
                 },
                 typeConflictResolver: (source: string[], target: string[]) => {
